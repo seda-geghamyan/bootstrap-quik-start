e8dae448376924a52cd936bc4dfe487a2d01ee37
Seda Geghamyan <seda.geghamyan@exio.tech>
2023-03-06 23:35:55 +0400

    Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-// import Alert from 'bootstrap/js/dist/alert'
+// import * as bootstrap from 'bootstrap'
+// import Modal from 'bootstrap/js/dist/modal'
 
 // or, specify which plugins you need:
-// import { Tooltip, Toast, Popover } from 'bootstrap'
+import { Tooltip, Toast, Popover, Modal } from 'bootstrap'
